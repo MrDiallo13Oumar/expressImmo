b5b85b939cf3068aa03580c44fa5cdb9ad671d71
Diallo Oumar <do25595@gmail.com>
2025-01-06 14:41:33 +0000

    verification

--- src/app/admin/components/contrat/_components/list-contrat/list-contrat.component.ts
+++ src/app/admin/components/contrat/_components/list-contrat/list-contrat.component.ts
@@ -4,6 +4,10 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { AddContratComponent } from '../../dialogs/add-contrat/add-contrat.component';
+import { ContratService } from '../../_services/contrat.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { convertObjectInFormData } from 'src/app/app.component';
+import { DeletePopupComponent } from 'src/app/shared/dialogs/delete-popup/delete-popup.component';
 
 @Component({
   selector: 'app-list-contrat',
@@ -28,7 +32,8 @@ displayedColumns: string[] = ['id', 'name', 'status', 'date', 'price', 'action']
   }
 
   constructor (private dialog : MatDialog ,
-
+                private service :ContratService,
+                private snackBar : MatSnackBar
 ){}
 
 @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -45,37 +50,86 @@ applyFilter (event: Event) {
    this.dataSource.paginator.firstPage()
  }
 }
+ngOnInit() {
+  this.getContrat()
+ }
+getContrat () {
+   this.service.getall('contrat', 'readAll.php').subscribe({
+     next: (reponse: any) => {
+        console.log('REPONSE SUCCESS : ', reponse)
+       this.dataSource.data = reponse
+     },
+     error: (err: any) => {
+       console.log('REPONSE ERROR : ', err)
+     }
+   })
+ }
 
   openDialog() {
     this.dialog.open(AddContratComponent, {
-     }) //.afterClosed()
-      // .subscribe((result) => {
-      //   if (result?.event && result.event === "insert") {
-      //     // console.log(result.data);
-      //      const formData = convertObjectInFormData(result.data);
-      //     this.dataSource.data.splice(0, this.dataSource.data.length);
-      //     //Envoyer dans la Base
-      //     this.service.create('engins','create.php', formData).subscribe({
-      //       next: (response) => {
-      //         this.snackBar.open("Engins enregistré avec succès !", "Okay", {
-      //           duration: 3000,
-      //           horizontalPosition: "right",
-      //           verticalPosition: "top",
-      //           panelClass: ['bg-success', 'text-white']
+     }) .afterClosed()
+      .subscribe((result) => {
+        if (result?.event && result.event === "insert") {
+          // console.log(result.data);
+           const formData = convertObjectInFormData(result.data);
+          this.dataSource.data.splice(0, this.dataSource.data.length);
+          //Envoyer dans la Base
+          this.service.create('contrat','create.php', formData).subscribe({
+            next: (response) => {
+              this.snackBar.open("Contrat enregistré avec succès !", "Okay", {
+                duration: 3000,
+                horizontalPosition: "right",
+                verticalPosition: "top",
+                panelClass: ['bg-success', 'text-white']
 
-      //         })
-      //         this.getEngins()
-      //       },
-      //       error: (err: any) => {
-      //         this.snackBar.open("Echec de l'ajout !", "Okay", {
-      //           duration: 3000,
-      //           horizontalPosition: "right",
-      //           verticalPosition: "top",
-      //           panelClass: ['bg-danger', 'text-white']
-      //         })
-      //       }
-      //     })
-      //   }
-     // })
+              })
+              this.getContrat()
+            },
+            error: (err: any) => {
+              this.snackBar.open("Echec de l'ajout !", "Okay", {
+                duration: 3000,
+                horizontalPosition: "right",
+                verticalPosition: "top",
+                panelClass: ['bg-danger', 'text-white']
+              })
+            }
+          })
+        }
+     })
   }
+
+    // DELETE
+    deleteFunction(id: any, table: string) {
+      this.dialog
+        .open(DeletePopupComponent, {
+          disableClose: true,
+          data: {
+            title: 'Suppression demandée!',
+            message: 'Voulez-vous vraiment supprimer cet élément ?',
+            messageNo: 'Non ?',
+            messageYes: 'Oui, Confirmer !',
+          },
+        })
+        .afterClosed()
+        .subscribe((data: any) => {
+          if (data) {
+            this.service.delete('public', 'delete.php', table, id).subscribe({
+              next: (response: any) => {
+                const messageClass =
+                  response.status == 1 ? ['bg-success', 'text-white'] : ['bg-danger', 'text-white'];
+                this.snackBar.open(response.message, 'Okay', {
+                  duration: 3000,
+                  horizontalPosition: 'right',
+                  verticalPosition: 'top',
+                  panelClass: messageClass,
+                });
+              },
+              error: (err: any) => {
+                console.error('Error : ', err);
+              },
+            });
+            this.getContrat()
+          }
+        });
+    }
 }
--- src/app/admin/components/contrat/dialogs/add-contrat/add-contrat.component.ts
+++ src/app/admin/components/contrat/dialogs/add-contrat/add-contrat.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Optional } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-contrat',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AddContratComponent {
 
+  Contrat = new FormGroup({
+    reservation_id: new FormControl(''),
+    date_signature: new FormControl(''),
+    montant_total: new FormControl(''),
+    statut :new FormControl(''),
+
+  })
+
+  constructor(
+    public dialogRef: MatDialogRef<AddContratComponent>,
+    @Optional() @Inject(MAT_DIALOG_DATA) public data: any,
+
+  ) { }
+  saveDataContrat() {
+    if (this.Contrat.valid) {
+      this.dialogRef.close({
+        event: "insert",
+        data: this.Contrat.value
+      })
+    }
+  }
 }
