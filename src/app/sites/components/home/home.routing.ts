e27c92fd5fac2ecab522a45c3721204c123fbf78
Diallo Oumar <do25595@gmail.com>
2025-01-06 17:29:33 +0000

    veriff

--- src/app/sites/components/home/home.routing.ts
+++ src/app/sites/components/home/home.routing.ts
@@ -1,11 +1,9 @@
 import { Routes } from "@angular/router";
 import { AccueilComponent } from "./accueil/accueil.component";
 import { ProprieteComponent } from "./propriete/propriete.component";
-<<<<<<< HEAD
-=======
 import { ServiceComponent } from "./service/service.component";
 import { ContactComponent } from "./contact/contact.component";
->>>>>>> c3282498850611a3c303d59ae814909053e0266a
+
 
 export const homeRouting: Routes = [
 
@@ -14,18 +12,13 @@ export const homeRouting: Routes = [
     component: AccueilComponent,
 
   },
-<<<<<<< HEAD
 
-
-=======
->>>>>>> c3282498850611a3c303d59ae814909053e0266a
   {
     path: 'propriete',
     component: ProprieteComponent,
 
   },
-<<<<<<< HEAD
-=======
+
   {
     path: 'service',
     component: ServiceComponent,
@@ -36,16 +29,12 @@ export const homeRouting: Routes = [
     component: ContactComponent,
 
   },
-  
->>>>>>> c3282498850611a3c303d59ae814909053e0266a
+
+
   // {
   //   path: 'login',
   //   component: LoginComponent,
   //  // canActivate: [GuardGuard]
   // },
-<<<<<<< HEAD
 
-
-=======
->>>>>>> c3282498850611a3c303d59ae814909053e0266a
 ]
