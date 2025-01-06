c3282498850611a3c303d59ae814909053e0266a
Thierno Mamoudou Bah <thiernomahmudbah@gmail.com>
2025-01-06 15:30:51 +0000

    cheick commit

--- src/app/admin/components/contrat/dialogs/add-contrat/add-contrat.component.ts
+++ src/app/admin/components/contrat/dialogs/add-contrat/add-contrat.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-contrat',
   templateUrl: './add-contrat.component.html',
   styleUrls: ['./add-contrat.component.scss']
 })
-export class AddContratComponent {
+export class AddContratComponent implements OnInit{
+
+ngOnInit(): void {
+  
+}
+
+OnAjouter(){
+  
+}
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,42 @@ import { filter } from 'rxjs';
 })
 export class AppComponent {
   title = 'expressimmo';
-}
+
+
+
+    canShowMenu = true;
+
+    constructor(public location: Location, private router: Router) {
+      // if ( this.router.url === '/') {
+      //   this.router.navigate(['/home/accueil']);
+      // }
+
+      this.router.events
+        .pipe(filter((event: any) => event instanceof NavigationEnd))
+        .subscribe((event: NavigationEnd) => {
+          const hiddenExactUrls = ['/hoomeAdmin/login'];
+          const hiddenModuleUrls = ['/home'];
+
+          // Vérification des URL exactes à cacher
+          if (hiddenExactUrls.includes(event.url)) {
+            this.canShowMenu = false;
+            return;
+          }
+
+          // Vérification si l'URL commence par un chemin spécifique (module /home)
+          if (hiddenModuleUrls.some((path) => event.url.startsWith(path))) {
+            this.canShowMenu = false;
+            return;
+          }
+
+          // Afficher le menu pour toutes les autres URLs
+          this.canShowMenu = true;
+        });
+    }
+  }
+
+
+
+
+
+
