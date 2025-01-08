277b270b3d8a130a9896638a37b5ba15b8bab041
Diallo Oumar <do25595@gmail.com>
2025-01-08 13:43:47 +0000

    avant integration nabylaye

--- src/app/admin/components/propriete/_component/details-propriete/details-propriete.component.ts
+++ src/app/admin/components/propriete/_component/details-propriete/details-propriete.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { PartenaireService } from '../../../partenaire/services/partenaire.service';
 
 @Component({
   selector: 'app-details-propriete',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./details-propriete.component.scss']
 })
 export class DetailsProprieteComponent {
+ Propriete = new FormGroup({
+    partenaire_id :new FormControl(''),
+    libelle: new FormControl(''),
+    adresse: new FormControl(''),
+    description: new FormControl(''),
+    etat :new FormControl(''),
+    disponible :new FormControl(''),
+    prix_journalier :new FormControl(''),
+    prix_mensuel :new FormControl(''),
 
+  })
+ constructor(
+  private service :PartenaireService
+ ){
+
+ }
+
+  ngOnInit(){
+    this.getPartenaire()
+  }
+
+  Partenaire : any =[]
+   getPartenaire () {
+     this.service.getall('partenaire', 'readAll.php').subscribe({
+       next: (reponse: any) => {
+          console.log('REPONSE SUCCESS : ', reponse)
+         this.Partenaire = reponse
+
+       },
+       error: (err: any) => {
+         console.log('REPONSE ERROR : ', err)
+       }
+     })
+   }
 }
