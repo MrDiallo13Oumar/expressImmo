d10bb3714ef8719af61e4a1e78d1ae1c6b58994d
Thierno Mamoudou Bah <thiernomahmudbah@gmail.com>
2025-01-20 11:45:53 +0000

    commit pour oumar

--- src/app/sites/components/home/accueil/accueil.component.ts
+++ src/app/sites/components/home/accueil/accueil.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProprieteService } from 'src/app/admin/components/propriete/_services/propriete.service';
+import { Propriete } from 'src/assets/Models/propriete';
 
 @Component({
   selector: 'app-accueil',
@@ -6,6 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./accueil.component.scss']
 })
 export class AccueilComponent {
+constructor(private router : Router, private activeroute : ActivatedRoute,private propriete : ProprieteService
+){}
+
+  data !: Propriete[]
+  infoPropriete: any = {};
+  getPropriete () {
+    this.propriete.getall('propriete', 'readAll.php').subscribe({
+      next: (reponse: any) => {
+        console.log('REPONSE SUCCESS : ', reponse)
+        this.data = reponse
+        // console.log("Data de cheick", this.data);
+        
+      },
+      error: (err: any) => {
+        console.log('REPONSE ERROR : ', err)
+      }
+    })
+  }
 
   scrollToTop(): void {
     window.scrollTo({ top: 0, behavior: 'smooth' }); // Le 'smooth' permet un défilement fluide
