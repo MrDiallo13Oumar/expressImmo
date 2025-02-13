2123d0051e106cd22611289b10e39b359fe8fc21
Diallo Oumar <do25595@gmail.com>
2025-02-13 11:20:29 +0000

    deploiement

--- src/app/admin/components/homeAdmin/_component/login/login.component.ts
+++ src/app/admin/components/homeAdmin/_component/login/login.component.ts
@@ -1,6 +1,7 @@
 
 import { Component } from '@angular/core';
 import { Validators, FormBuilder } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { AuthService } from 'src/app/admin/guards/service/auth.service';
 import { convertObjectInFormData } from 'src/app/app.component';
 
@@ -18,26 +19,48 @@ export class LoginComponent {
 
   constructor(
     private loginService: AuthService,
+    private snackBar :MatSnackBar,
     private fb: FormBuilder,
   ) { }
 
   onSubmit(): void {
-    // console.log(this.loginForm.value);
-    const formData = this.loginForm.value;
-    this.loginService
-      .login(
-        'authentification',
-        'login.php',
-        convertObjectInFormData(this.loginForm.value)
-      )
-      .subscribe({
-        next: (response: any) => {
-          this.loginService.saveToken(response.access_token, response.idUser);
-        },
-        error: (error: any) => {
-          console.log('ERROR : ', error);
-        },
-      });
+    // Récupération des données du formulaire
+    const formData = convertObjectInFormData(this.loginForm.value);
+
+    // Appel du service de connexion
+    this.loginService.login('authentification', 'login.php', formData).subscribe({
+      next: (response: any) => {
+        // Enregistrer le token et l'ID utilisateur
+        this.loginService.saveToken(response.access_token, response.idUser);
+
+        // Afficher un message de succès si disponible
+        if (response?.message) {
+          this.snackBar.open(response.message, 'Okay', {
+            duration: 3000,
+            horizontalPosition: 'right',
+            verticalPosition: 'top',
+            panelClass: ['bg-success', 'text-white'],
+          });
+        }
+      },
+      error: (err: any) => {
+        // Gestion des erreurs
+        console.error('Erreur :', err);
+        const errorMessage =
+          err.error?.message || "Une erreur s'est produite lors de la connexion.";
+
+        // Affichage du message d'erreur
+        this.snackBar.open(errorMessage, 'Okay', {
+          duration: 3000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top',
+          panelClass: ['bg-danger', 'text-white'],
+        });
+      },
+    });
   }
 
+
 }
+
+
